fbbe4462d5480090863ea6f202d20de077367ae5
dgichiri <dgichiri@gmail.com>
2025-01-17 17:31:23 +0300

    Update EXPERIENCE.py

--- EXPERIENCE.py
+++ EXPERIENCE.py
@@ -15,16 +15,15 @@ st.write("""
         
          """)
 
-msgs = [ "Success is not the key to happiness. Happiness is the key to success. If you love what you are doing, you will be successful.",
-    "The only way to do great work is to love what you do.",
-    "Success usually comes to those who are too busy to be looking for it.",
-    "Don't be afraid to give up the good to go for the great.",
-    "I find that the harder I work, the more luck I seem to have.",
-    "Success is not in what you have, but who you are.",
-    "The road to success and the road to failure are almost exactly the same.",
-    "Success is not how high you have climbed, but how you make a positive difference to the world.",
-    "Success is not the absence of failure; it's the persistence through failure.",
-    "The only limit to our realization of tomorrow is our doubts of today.",
+msgs = [ "Welcome to Mombasa, the vibrant coastal gem of Kenya, where the Indian Ocean meets rich history, culture, and endless possibilities. Over the next two days, we gather not just in the heart of this iconic city but at the crossroads of innovation, collaboration, and purpose.
+
+As you explore Mombasa, you’ll encounter its timeless monuments—the majestic Fort Jesus, a testament to resilience and transformation, and the enchanting Old Town, where centuries of trade and connection have shaped a thriving community. These landmarks remind us of the power of vision, perseverance, and the ability to adapt while staying rooted in purpose.
+
+This forum is more than a meeting; it is a call to connect, engage, and inspire. Together, we will delve into the bold vision of becoming Africa’s leading purpose-driven technology company by 2030. We are not just building a business; we are creating a legacy that empowers communities, drives sustainable growth, and harnesses technology as a force for good.
+
+Your presence here signifies a shared belief in this vision. Over the next two days, let us challenge assumptions, spark innovation, and forge partnerships that will redefine what is possible. Let us draw inspiration from Mombasa’s rich history and use it to fuel our journey toward a future where technology serves humanity and Africa leads the way.
+
+Thank you for joining us. Let’s make these two days transformative..",
 
 
                      
